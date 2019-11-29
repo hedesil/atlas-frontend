2679f76b8583c12fe571d02e09f0762d71a9967e
Mariano Moreno <mariano25294@gmail.com>
2019-11-29 20:07:43 +0100

    - Prepared test component for backend. - Deleted br

--- src/app/tests/tests.component.ts
+++ src/app/tests/tests.component.ts
@@ -1,16 +1,25 @@
 import {Component, OnInit} from '@angular/core';
 
+export interface Test {
+  name: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-tests',
   templateUrl: './tests.component.html',
   styleUrls: ['./tests.component.scss']
 })
+
 export class TestsComponent implements OnInit {
   isModalVisible: boolean;
   descSort;
   testsList;
   total;
+  newTest: Test = {description: "", name: ""};
+
   constructor() {
+
   }
 
   ngOnInit() {
@@ -19,4 +28,15 @@ export class TestsComponent implements OnInit {
   deleteTest(test) {
   }
 
+  closeModal() {
+    this.isModalVisible = false;
+    this.newTest = {
+      name: '',
+      description: ''
+    };
+  }
+
+  addTest() {
+
+  }
 }
