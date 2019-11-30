166062189dba9a3bbf215f0db40df0fc78fabddf
Mariano Moreno <mariano25294@gmail.com>
2019-11-30 01:45:24 +0100

    - Fixed merge 2

--- src/app/bar-grafic/bar-grafic.component.ts
+++ src/app/bar-grafic/bar-grafic.component.ts
@@ -1,54 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 //import * as Chartist from 'chartist';
-import { ChartDataSets, ChartOptions,ChartType } from 'chart.js';
-import { SingleDataSet, Label, Color, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+import {ChartDataSets, ChartOptions, ChartType} from 'chart.js';
+import {SingleDataSet, Label, Color, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip} from 'ng2-charts';
+
 @Component({
   selector: 'app-bar-grafic',
   templateUrl: './bar-grafic.component.html',
   styleUrls: ['./bar-grafic.component.scss']
 })
 export class BarGraficComponent implements OnInit {
-   public pieChartOptions: ChartOptions = {
+  public pieChartOptions: ChartOptions = {
     responsive: true,
-    };
+  };
 
-    constructor() {
+
+  constructor() {
     monkeyPatchChartJsTooltip();
     monkeyPatchChartJsLegend();
-
   }
 
   public options: ChartType;
-  public verSeleccion: string        = '';
-  public seleccion : ChartType = 'pie';
+  public verSeleccion: string = '';
+  public seleccion: ChartType = 'bar';
   // Pie
-   
 
-  public pieChartLabels: Label[] 
-  public pieChartData: SingleDataSet 
-  public pieChartType: ChartType 
+
+  public pieChartLabels: Label[];
+  public pieChartData: SingleDataSet;
+  public pieChartType: ChartType;
   public chartReady = true;
   public pieChartLegend = true;
-  public pieChartPlugins = []
+  public pieChartPlugins = [];
 
- ngOnInit(){
- 
-  this.pintargrafica();
+  ngOnInit() {
 
- }
- pintargrafica(){
- //this.pieChartOptions: ChartOptions = {
- // responsive: true,
- //};
-  this.pieChartLabels = [['Criticidad', 'Critica'], ['Criticidad', 'Alta'], ['Criticidad', 'Medio' ], 'Criticidad Baja'];
-  this.pieChartData = [4, 9, 17, 35];
-  this.pieChartType = this.seleccion;
-  this.chartReady = true;
-  this.pieChartLegend = true;
-  this.pieChartPlugins = []
- }
+    this.pintargrafica();
 
-  cambiagrafico(options){
+  }
+
+  pintargrafica() {
+    //this.pieChartOptions: ChartOptions = {
+    // responsive: true,
+    //};
+    this.pieChartLabels = [['Criticidad', 'Critica'], ['Criticidad', 'Alta'], ['Criticidad', 'Medio'], 'Criticidad Baja'];
+    this.pieChartData = [4, 9, 17, 35];
+    this.pieChartType = this.seleccion;
+    this.chartReady = true;
+    this.pieChartLegend = true;
+    this.pieChartPlugins = [];
+  }
+
+  cambiagrafico(options) {
     this.seleccion = options;
     this.pieChartType = this.seleccion;
   }
